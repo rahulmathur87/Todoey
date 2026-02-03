4ba9e3f681ec42789972b6fe2ed1d99f750028d4
Rahul <rahulmathur87@gmail.com>
2026-02-03 13:50:09 +0530

    Todoey Start

---
+++ lib/screens/tasks_screens.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        shape: CircleBorder(),
+        onPressed: null,
+        child: Icon(Icons.add, color: Colors.white,),
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                top: 60,
+                left: 30,
+                right: 30,
+                bottom: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CircleAvatar(
+                    radius: 30,
+                    child: Icon(
+                      Icons.list,
+                      size: 30,
+                      color: Colors.lightBlueAccent,
+                    ),
+                  ),
+                  const Text(
+                    'Todoey',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const Text(
+                    '12 Tasks',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

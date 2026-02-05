162ee842a00d75a9c94e698c6f5641a203b1858e
Rahul <rahulmathur87@gmail.com>
2026-02-05 15:25:27 +0530

    made checkboxes functional by refactoring them to stateful widget

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
-  const TaskTile({
-    super.key,
-  });
+  const TaskTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text('This is a task.'),
-      trailing: Checkbox(value: false, onChanged: null),
-    );
+    return ListTile(title: Text('This is a task.'), trailing: TaskCheckBox());
+  }
+}
+
+class TaskCheckBox extends StatefulWidget {
+  const TaskCheckBox({super.key});
+
+  @override
+  State<TaskCheckBox> createState() => _TaskCheckBoxState();
+}
+
+class _TaskCheckBoxState extends State<TaskCheckBox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(activeColor: Colors.lightBlueAccent, value: isChecked, onChanged: (newValue) {
+      setState(() {
+        isChecked = newValue!;
+      });
+    });
   }
 }
